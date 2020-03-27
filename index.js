16f15755dd96effe765767aa731a5bd91c3d651f
yasharonquillo <yasharon@hawaii.edu>
2020-03-27 00:29:03 +0000

    added comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 var _ = require('lodash');
 const https = require("https");
 
+//starting contents from Udemy
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//not necessary but this is a function that can truncate text
 text_truncate = function(str, length, ending) {
     if (length == null) {
       length = 100;
@@ -96,17 +98,19 @@ app.post("/compose", function(req,res){
 
 
 
-
+//global array for weathers and inputs will be pushed in here
 let weathers = [];
 
-
+//getting the weather page
 app.get("/weather", function(req,res){
   res.render("weather", {
     weathers:weathers
   });
 });
 
+//posting the weather inputs
 app.post("/weather", function(req,res){
+//weathers array will be cleared everytime a post is made
 weathers = [];
       const city = req.body.city;
       const units = "imperial";
@@ -133,6 +137,7 @@ weathers = [];
               const icon = weatherData.weather[0].icon;
               const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
+            //object that will be pushed back to weathers array and to weather.ejs
               const weather = {
                 city: city,
                 image: imageURL,
@@ -161,7 +166,7 @@ weathers = [];
 
 });
 });
-weathers = [];
+
 
 
 
